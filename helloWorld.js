ef06202f4a567b17f2cf843e637a8287ef7ea2d5
lk-4-class-use <62731730+lk-4-class-use@users.noreply.github.com>
2020-05-18 04:39:10 -0500

    Formatted displayed message

--- helloWorld.js
+++ helloWorld.js
@@ -1,7 +1,9 @@
+// Version 1. Lecture video code.
+
 // (1) import the http module, and
 // (2) assign that module to the http variable.
-var http = require('http');
 
+var http = require('http');
 // (3) createServer takes one argument -- a function, event-listener
 // (4) event listener will take two argument every time a request is sent in
 //     by the server a request object and a response object.
@@ -10,14 +12,16 @@ var http = require('http');
 http.createServer(function(request,response){
 // writeHead sets the status code and lets us set header information.
 // In this case, we are sending back plain, unformatted text.
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  // response.writeHead(200, { 'Content-Type': 'text/plain' });
 // end says that after the data inside the function is sent,
 // we are done and the response is complete.
-  response.end('Hello world!');
+
+  // response.end('Hello world!');
 
 // // Send HTML instead:
-// response.writeHead(200, {'Content-Type': 'text/html'});
-// response.end('<h2>Hello world!</h2> <p>Hello node!</p>');
+response.writeHead(200, {'Content-Type': 'text/html'});
+response.end('<h2>Hello world!</h2> <p>Hello node!</p>');
 
 // Once the server is created, we call the listen method
 // and provide the port we want it to listen on
